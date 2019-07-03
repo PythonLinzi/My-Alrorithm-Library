6790baf7c8a283b9d3e378644ba0ca057258c80c
Minkowski_Lin <37980981+PythonLinzi@users.noreply.github.com>
2019-07-03 23:27:28 +0800

    Create BH-Multiple Demension.py

---
+++ BH-Basin Hopping Algorithm/BH-Multiple Demension.py
@@ -0,0 +1,64 @@
+import numpy as np
+from scipy.optimize import basinhopping
+
+''' take two-D as example '''
+# demo1: compute directly, without gradient
+def func(x):
+    return np.cos(14.5 * x[0] - 0.3) + (x[1] + 0.2) * x[1] + (x[0] + 0.2) * x[0]
+
+x0 = np.array([1.0, 1.0]) # initial guess
+minimizer_kwargs = {"method": "L-BFGS-B"}
+ans = basinhopping(func=func, x0=x0, niter=300, stepsize=0.5,
+                   minimizer_kwargs=minimizer_kwargs)
+print("Global minimum: x = [%.4f, %.4f], f(%.4f, %.4f) = %.4f" % (ans.x[0], ans.x[1], ans.x[0], ans.x[1], ans.fun))
+
+
+'''----------------------------------------------------分割线--------------------------------------------------------'''
+# demo2 : use gradient information to speed up computation -- jac: True
+def func(x):
+    f = np.cos(14.5 * x[0] - 0.3) + (x[1] + 0.2) * x[1] + (x[0] + 0.2) * x[0]
+    df = np.zeros(2)
+    df[0] = -14.5 * np.sin(14.5 * x[0] - 0.3) + 2. * x[0] + 0.2 # x1的偏导
+    df[1] = 2. * x[1] + 0.2 # x2的偏导
+    return f, df
+
+x0 = np.array([1.0, 1.0]) # initial guess
+minimizer_kwargs = {"method": "L-BFGS-B", "jac": True}
+ans = basinhopping(func=func, x0=x0, niter=300, stepsize=0.5,
+                   minimizer_kwargs=minimizer_kwargs)
+print("Global minimum: x = [%.4f, %.4f], f(%.4f, %.4f) = %.4f" % (ans.x[0], ans.x[1], ans.x[0], ans.x[1], ans.fun))
+
+
+'''----------------------------------------------------分割线--------------------------------------------------------'''
+# demo3: set take_step and bounds for differenr varible
+class TakeStep(object):
+    def __init__(self, stepsize=0.5):
+        self.stepsize = stepsize
+    def __call__(self, x: np.ndarray):
+        s = self.stepsize
+        x[0] += np.random.uniform(-2 * s, 2 * s)
+        x[1:] += np.random.uniform(-s, s, x[1:].shape)
+        return x
+
+class Bounds(object):
+    def __init__(self, xmax=[1.1,1.1], xmin=[-1.1,-1.1]):
+        self.xmax, self.xmin = np.array(xmax), np.array(xmin)
+    def __call__(self, **kwargs):
+        x = l=kwargs["x_new"]
+        tmax, tmin = bool(np.all(x <= self.xmax)), bool(np.all(x >= self.xmin))
+        return tmax and tmin
+
+
+def func(x):
+    f = np.cos(14.5 * x[0] - 0.3) + (x[1] + 0.2) * x[1] + (x[0] + 0.2) * x[0]
+    df = np.zeros(2)
+    df[0] = -14.5 * np.sin(14.5 * x[0] - 0.3) + 2. * x[0] + 0.2 # x1的偏导
+    df[1] = 2. * x[1] + 0.2 # x2的偏导
+    return f, df
+
+takstep, bnds = TakeStep(), Bounds()
+x0 = np.array([1.0, 1.0]) # initial guess
+minimizer_kwargs = {"method": "L-BFGS-B", "jac": True}
+ans = basinhopping(func=func, x0=x0, niter=300, minimizer_kwargs=minimizer_kwargs,
+                   take_step=takstep, accept_test=bnds)
+print("Global minimum: x = [%.4f, %.4f], f(%.4f, %.4f) = %.4f" % (ans.x[0], ans.x[1], ans.x[0], ans.x[1], ans.fun))
