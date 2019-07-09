27d45075aa8559e74a9936d0c5fe20974d22abe8
Minkowski_Lin <37980981+PythonLinzi@users.noreply.github.com>
2019-07-09 20:02:42 +0800

    Create ti12_1.m

---
+++ GA-Genetic Algorithm/MATLAB牛逼！！！/ti12_1.m
@@ -0,0 +1,13 @@
+function ti12_1
+fun1=@(x) (x(1)-2)^2+(x(2)-1)^2;
+a=[-1,2]; b=1;
+[x, y] = ga(fun1,2,a,b,[],[],[],[],@fun2);
+fprintf('\nansX = ');disp(x);
+fprintf('ansY = %d\n', y);
+function [c,ceq]=fun2(x)
+c=-x(1)^2/4+x(2)^2-1; ceq=[];
+% [x, y] = ga(@fitnessfunc, nvaes, A, b, Aeq, beq, LB, UB, @nonlcon, options)
+% A*X <= b
+% Aeq*X = beq
+% LB<X<UB
+% nonlcon: nonlinear constrains
