ce9c271c87684f4f5fe55e6b4e2570385b579e5d
Minkowski_Lin <37980981+PythonLinzi@users.noreply.github.com>
2019-08-24 20:39:27 +0800

    Update BH-Constrains.py

--- BH-Basin Hopping Algorithm/BH-Constrains.py
+++ BH-Basin Hopping Algorithm/BH-Constrains.py
@@ -1,10 +1,12 @@
 '''
-多运行几次
-注意检查结果是否满足约束条件
+To improve your chances of finding a global minimum:
+ *use higher popsize values,
+ *with higher mutation and (dithering),
+ *but lower recombination values.
+ This has the effect of widening the search radius, but slowing convergence.
 '''
-
 import numpy as np
-from scipy.optimize import basinhopping as BH
+from scipy.optimize import differential_evolution as DE
 
 
 def target_func(x:np.ndarray):
@@ -15,7 +17,7 @@ def f(x:np.ndarray):
     ''' constrains <= 0 '''
     ''' 采用罚函数法将约束条件加入目标函数 '''
     y = target_func(x)
-    penelty = 0x3f3f3f3f # 惩罚系数(可以适当调整)
+    penelty = 1e30 # 惩罚系数
     bnds = -x[0] * x[0] + x[1] - x[2] * x[2]  # -x1^2 + x2 - x3^2 <= 0
     if bnds > 0: y += penelty * bnds # violation of constrains
     bnds = x[0] + x[1] * x[1] + x[2] * x[2] - 20  # x1 + x2^2 + x3^2 - 20 <= 0
@@ -26,9 +28,7 @@ def f(x:np.ndarray):
     if bnds > 0: y += penelty * bnds
     return y
 
-
-x0 = np.random.rand(3) # initial guess
-mk = {"method": "L-BFGS-B"}
-res = BH(func=f, x0=x0, niter=500, stepsize=0.5,
-         minimizer_kwargs=mk)
+bnds = [(0, 100), (0, 100), (0, 100)]
+res = DE(func=f, bounds=bnds, maxiter=1000, popsize=25,
+         mutation=(0.5, 1), recombination=0.7, tol=0.01)
 print("Global Minimum: xmin = {0}, f(xmin) = {1:.6f}".format(res.x, res.fun))
