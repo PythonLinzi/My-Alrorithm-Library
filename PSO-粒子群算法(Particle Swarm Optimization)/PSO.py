b0a667bd6816ea9165d4ceb8c2c999bda6f56022
Minkowski_Lin <37980981+PythonLinzi@users.noreply.github.com>
2019-08-24 20:33:24 +0800

    Update and rename pso.py to PSO.py

--- PSO-粒子群算法(Particle Swarm Optimization)/PSO.py
+++ PSO-粒子群算法(Particle Swarm Optimization)/PSO.py
@@ -23,45 +23,59 @@ def Bounds(x):
 
 def f(x):
     y, bnds = target_func(x), Bounds(x)
-    penelty = 0x3f3f3f3f # 注意适当调整惩罚系数
+    penelty = np.inf # 惩罚系数
     for value in bnds:
         if value > 0: # violation of constrains
             y += (penelty * value)
     return y
 
 
-def PSO():
-    N, niter = 50, 500
-    vmin, vmax = -1, 1
+def PSO(N=50, niter=100, bnds=[-10, 10]):
     w, c1, c2 = 1, 2, 2.1
+    vmin, vmax = -1, 1
     c = c1 + c2
     K = 2 / (np.abs(2 - c - np.sqrt(c * c - 4 * c)))
-    bnds = [-10, 10]
     x, v = bnds[0] + (bnds[1] - bnds[0]) * rand(N), rand(N)
     y = np.array([f(v) for v in x])
-    pbest_x, pbest_y = x.copy(), y.copy()
-    gbest_x, gbest_y = x[y.argmin()], y.min()
+    pb_x, pb_y = x.copy(), y.copy()
+    gb_x, gb_y = x[y.argmin()], y.min()
+    GroupTime, GroupBest = [], [] # for plotting
     for _ in range(niter):
         for i in range(N):
-            v[i] = K * (v[i] + c1 * rand(1) * (pbest_x[i] - x[i]) + c2 * rand(1) * (gbest_x - x[i]))
+            pb = c1 * rand() * (pb_x[i] - x[i])
+            gb = c2 * rand() * (gb_x - x[i])
+            v[i] = K * (v[i] + pb + gb)
             v[i] = min(v[i], vmax)
             x[i] += max(v[i], vmin)
             y[i] = f(x[i])
-            if y[i] < pbest_y[i]:
-                pbest_x[i], pbest_y[i] = x[i], y[i]
-            if y[i] < gbest_y:
-                gbest_x, gbest_y = x[i], y[i]
-    print("Global Minimum: xmin = {0}, f(xmin) = {1:.6f}".format(gbest_x, gbest_y))
-    return gbest_x, gbest_y
+            if y[i] < pb_y[i]:
+                pb_x[i], pb_y[i] = x[i], y[i]
+            if y[i] < gb_y:
+                gbest_x, gb_y = x[i], y[i]
+        GroupTime.append(_ + 1)
+        GroupBest.append(gb_y)
+    s1 = "Global Minimum: xmin = {0}, "
+    s2 = "f(xmin) = {1:.6f}"
+    ss = s1 + s2
+    print(ss.format(gb_x, gb_y))
+    return [gb_x, gb_y], GroupTime, GroupBest
 
 from datetime import datetime
 s = datetime.now()
-PSO()
+res, gt, gy = PSO()
 e = datetime.now()
 print("Running Time:", e - s)
+import matplotlib.pyplot as plt
+plt.plot(gt, gy, label='finding process', c='r')
+plt.scatter(gt, gy, label='finding process', s=10)
+plt.xlabel('Iteration Time')
+plt.ylabel('Target')
+plt.title('Convergence')
+plt.grid()
+plt.show()
 
 
 '''
-Global Minimum: xmin = 6.484184832213667, f(xmin) = -1549.730940
-Running Time: 0:00:00.473265
+Global Minimum: xmin = 6.484184871679452, f(xmin) = -1549.730940
+Running Time: 0:00:00.079901
 '''
