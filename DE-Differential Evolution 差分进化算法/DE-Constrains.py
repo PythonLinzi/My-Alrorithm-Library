41ceed938f10f35c483063f064c69e6fd8f458d6
Minkowski_Lin <37980981+PythonLinzi@users.noreply.github.com>
2019-08-24 20:37:47 +0800

    Update DE-Constrains.py

--- DE-Differential Evolution 差分进化算法/DE-Constrains.py
+++ DE-Differential Evolution 差分进化算法/DE-Constrains.py
@@ -17,7 +17,7 @@ def f(x:np.ndarray):
     ''' constrains <= 0 '''
     ''' 采用罚函数法将约束条件加入目标函数 '''
     y = target_func(x)
-    penelty = 0x3f3f3f3f # 惩罚系数(可以适当调整)
+    penelty = 1e30 # 惩罚系数
     bnds = -x[0] * x[0] + x[1] - x[2] * x[2]  # -x1^2 + x2 - x3^2 <= 0
     if bnds > 0: y += penelty * bnds # violation of constrains
     bnds = x[0] + x[1] * x[1] + x[2] * x[2] - 20  # x1 + x2^2 + x3^2 - 20 <= 0
