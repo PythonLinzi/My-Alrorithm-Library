31993324a44c2fc7b60b2955ca6dc12434118fc1
Minkowski_Lin <37980981+PythonLinzi@users.noreply.github.com>
2019-08-26 01:37:43 +0800

    Update GA.py

--- GA-Genetic Algorithm/GA.py
+++ GA-Genetic Algorithm/GA.py
@@ -68,6 +68,7 @@ class GA():
 
     def __mutate(self, ch):
         '''
+        Mutation Process
         :param ch: child DNA
         :return: new child
         '''
@@ -76,17 +77,17 @@ class GA():
                 ch[point] = 1 if ch[point] == 0 else 0
         return ch
 
-    def compute(self, niter=100):
+    def compute(self, niter=100, plot=True, bf=None):
         '''
         Main Computing Loop
         :param niter: int, number of iterations
-                    number of generation
+        :param bf: Boundary Function
         :return: best X, and best y
         '''
         bestX = self.__Bin2Dec(self.pop[0])
         bestY = self.f(bestX)
-        t4plot = [0]
-        y4plot = [bestY]
+        t4plot = []
+        y4plot = []
         tx, ty = bestX, bestY
         ss = 'iter = {0},  y = {1}'
         for i in range(niter):
@@ -104,13 +105,19 @@ class GA():
                 pa = ch
             if ty < bestY:
                 bestX, bestY = tx, ty
-            t4plot.append(i + 1)
-            y4plot.append(bestY)
+            if bf:
+                if np.all(bf(bestX) <= 0):
+                    t4plot.append(i + 1)
+                    y4plot.append(bestY)
+            else:
+                t4plot.append(i + 1)
+                y4plot.append(bestY)
         s1 = "Global Minimum: xmin = {0}, "
         s2 = "f(xmin) = {1:.6f}"
         tmps = s1 + s2
         print(tmps.format(bestX, bestY))
-        self.__conver_plot(t4plot, y4plot)
+        if plot == True:
+            self.__conver_plot(t4plot, y4plot)
         return bestX, bestY
 
     def __conver_plot(self, x, y):
